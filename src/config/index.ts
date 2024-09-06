a48018b28650dc3f46ed8efe919573b37afc8370
Manoz <talk2manoz@gmail.com>
2024-09-06 11:51:40 +0545

    fix: medata for homepage

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,11 +3,11 @@ import { Metadata } from "next";
 export const SITE_CONFIG: Metadata = {
   title: {
     // write a default title for astra a ai powered website builder suggest something unique and catchy don't use the same words of ai powered website builder give a unique name
-    default: "Linkedin Copilot - AI Powered Website Builder",
+    default: "Linkedin Copilot - AI Powered Carousel Maker",
     template: `%s | Linkedin Copilot`,
   },
   description:
-    "Linkedin Copilot is an AI powered website builder that helps you create a website in minutes. No coding skills required. Get started for free!",
+    "Linkedin Copilot is an AI powered Carousel Maker that helps you create a website in minutes. No coding skills required. Get started for free!",
   icons: {
     icon: [
       {
